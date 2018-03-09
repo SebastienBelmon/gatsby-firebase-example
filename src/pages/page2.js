55401033982b4a9767770135829ef9de745a7a6c
nenu <nenu@nenu.fr>
2018-03-09 15:36:43 +0100

    working example of private Route

--- src/pages/page2.js
+++ src/pages/page2.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import withAuthorization from '../hoc/withAuthorization';
 
-const Page2 = props => {
+const Page2 = (props, { loggedUser }) => {
   return <div>Hello page 2</div>;
 };
 
-export default Page2;
+Page2.contextTypes = {
+  loggedUser: PropTypes.object,
+}
+
+//? c'est une fonction que l'on doit fournir au HOC
+//? pour savoir si on doit authorisé ou pas l'user
+//? à voir la page
+const authCondition = (loggedUser) => !!loggedUser;
+
+export default withAuthorization(Page2, authCondition);
